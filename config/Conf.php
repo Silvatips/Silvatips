d401bd4b0b6f164e364267a78dea0827a33d7d3f
Automgen <51507452+Automgen@users.noreply.github.com>
2019-08-08 21:51:57 +0200

    Update Conf.php
    Suppression des sauts de lignes inutiles.

--- config/Conf.php
+++ config/Conf.php
@@ -1,39 +1,39 @@
-<?php
-	class Conf{
-
-
-		static private $database = array(
-		    //Voltis: Je l'ai mis a jour
-			'hostname' => 'localhost',
-			'database' => 'id10168254_mtips1',
-			'login' => 'id10168254_mtips1',
-			'password' => 'Mtipscestsuper66');
-
-		//La variable debug est un boolean
-		static private $debug = True;
-
-		
-		//Getters & Setters
-
-		static public function getDebug(){
-			return self::$debug;
-		}
-
-		static public function getLogin(){
-			return self::$database['login'];
-		}
-
-		static public function getHostName(){
-			return self::$database['hostname'];
-		}
-
-		static public function getDataBase(){
-			return self::$database['database'];
-		}
-
-		static public function getPassword(){
-			return self::$database['password'];
-		}
-
-	}
+<?php
+	class Conf{
+
+		//Variables
+
+		static private $database = array(
+		    //Voltis: Je l'ai mis a jour
+			'hostname' => 'localhost',
+			'database' => 'id10168254_mtips1',
+			'login' => 'id10168254_mtips1',
+			'password' => 'Mtipscestsuper66');
+
+		//La variable debug est un boolean
+		static private $debug = True;
+
+
+		//Getters & Setters
+
+		static public function getDebug(){
+			return self::$debug;
+		}
+
+		static public function getLogin(){
+			return self::$database['login'];
+		}
+
+		static public function getHostName(){
+			return self::$database['hostname'];
+		}
+
+		static public function getDataBase(){
+			return self::$database['database'];
+		}
+
+		static public function getPassword(){
+			return self::$database['password'];
+		}
+	}
 ?>
